39fe92f0916578031ba1d50fca07d93db8eb5ca5
Austin King <aust.a.king@gmail.com>
2015-12-10 16:20:50 -0800

    Added .env functionality.

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
+require('dotenv').load({silent: true}); //loads environment variables defined in .env; for use in development; detects that .env exists
+
 var express = require('express');
 var app = express();
-var mongoose = require('mongoose');
 
-// mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/users_dev');
 process.env.APP_SECRET = process.env.APP_SECRET || 'for the love of zeus! Change Me!';
 
 app.use(express.static(__dirname + '/build'));
 
 var upcRouter = require(__dirname + '/routes/upc_routes');
 
-app.use('/api', upcRouter);
+app.use('/', upcRouter);
 
 app.use(function(req, res) {
   res.status(404).send('Page not found');
